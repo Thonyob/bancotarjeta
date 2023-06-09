3ab7abfb13d48e0902c90c75211a73d8bf39678e
Thonyob <anthonyolivaresb@gmail.com>
2023-06-08 21:29:18 -0500

    cambio

---
+++ src/main/java/com/bootcamp/bancotarjeta/BancoTarjetaApplication.java
@@ -0,0 +1,13 @@
+package com.bootcamp.bancotarjeta;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BancoTarjetaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BancoTarjetaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bootcamp/bancotarjeta/BancoTarjetaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bootcamp.bancotarjeta;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BancoTarjetaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
